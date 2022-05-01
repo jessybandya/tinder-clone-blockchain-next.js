ce7ceb561e8ea62398d5f8a42da32f227c3097e2
Uwimana Jessy Bandya <jessy.bandya5@gmail.com>
2022-05-01 23:39:23 +0300

    Second Complete.

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
 import '../styles/globals.css'
+import { MoralisProvider } from 'react-moralis'
+import { TinderProvider } from '../context/TinderContext'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <MoralisProvider
+      serverUrl='https://1n6yww3r0sdo.usemoralis.com:2053/server'
+      appId='rjnMZCXprSlpQmv50cYVtvrTWw72eYHY8WYQtrPX'
+    >
+      <TinderProvider>
+        <Component {...pageProps} />
+      </TinderProvider>
+    </MoralisProvider>
+  )
 }
 
 export default MyApp
